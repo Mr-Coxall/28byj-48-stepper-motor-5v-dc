db0a192b7053aa1866479586172912c41f4b35d5
Patrick Coxall <mr.coxall@mths.ca>
2020-07-06 21:30:31 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,6 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.House)
-    forward = true
-    basic.pause(2500)
-    forward = false
-    basic.showIcon(IconNames.Yes)
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        robotbit.StepperDual(90, 90)
+    }
 })
-let forward = false
 basic.showIcon(IconNames.Yes)
-forward = false
-basic.forever(function () {
-    if (forward == true) {
-        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
-    }
-})
-basic.forever(function () {
-    if (forward == true) {
-        robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
-    }
-})
